c3da1b4e7e0c7d6518368b801a3b7a0a6ad14694
Travis Lawrence <travis.lawrence12@gmail.com>
2014-01-21 09:23:49 -0800

    update significant content

--- public/js/application.js
+++ public/js/application.js
@@ -1,12 +1,43 @@
-$('a[href*=#]:not([href=#])').click(function() {
-  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname) {
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-    if (target.length) {
-      $('html,body').animate({
-        scrollTop: target.offset().top
-      }, 1000);
-      return false;
-    }
-  }
+$('#nav a').click(function(e){
+    var $targetName = $(this).attr('href').slice(1);
+    var $target = $('.cycle, .instruct').find('div[id=' + $targetName + ']');
+
+    var $targetOffset = $target.offset().top;
+    $('html,body')
+    .animate({
+        scrollTop: $targetOffset
+    }, 500, "easeOutQuad", function(){
+        $target.next('a.back-to-top').hide();
+    });
+        e.preventDefault();
 });
+
+// $('a.back-to-top').click(function(e){
+//     var $H1 = $('h1').offset().top;
+
+//     $('html,body')
+//     .animate({
+//         scrollTop: $H1
+//     }, 300, "easeInQuad");
+//      e.preventDefault();
+// });
+
+
+
+
+
+// <h1>Smooth scrolling</h1>
+
+// <div id="links">
+//      <a href="#link-one">Link one</a>
+//      <a href="#link-two">Link two</a>
+//      <a href="#link-three">Link three</a>
+//      <a href="#link-four">Link four</a>
+// </div>
+
+// <div class="content-blocks">
+//     <div class="content" id="link-one"> This is the first block <br>(<a class="back-to-top" href="#">Back to top</a>)</div>
+//     <div class="content" id="link-two"> This is the second block <br>(<a class="back-to-top" href="#">Back to top</a>)</div>
+//     <div class="content" id="link-three"> This is the third block <br>(<a class="back-to-top" href="#">Back to top</a>)</div>
+//     <div class="content" id="link-four"> This is the fourth block <br>(<a class="back-to-top" href="#">Back to top</a>)</div>
+// </div>
