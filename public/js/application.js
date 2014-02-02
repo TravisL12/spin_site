9510815225b55c8c19720d5c1654789a46efe00f
Travis Lawrence <travis.lawrence12@gmail.com>
2014-02-01 21:41:47 -0800

    big changes to email form

--- public/js/application.js
+++ public/js/application.js
@@ -3,7 +3,6 @@ var royoApp = angular.module('royoApp', []);
 royoApp.controller('royoController', ['$scope', '$filter', function($scope, $filter) {
 
     $scope.cyclist    = {type: "Cyclist", placehold: "cyclist@royo.com"}
-    $scope.instructor = {type: "Instructor", placehold: "instructor@royo.com"}
 
     $scope.emailvalid = function(email){
         console.log(email)
