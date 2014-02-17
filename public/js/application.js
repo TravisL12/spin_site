486072f0ab705ab97f8ae9015588cec64cdab811
Travis Lawrence <travis.lawrence12@gmail.com>
2014-02-17 11:16:01 -0800

    mandrill API sending email

--- public/js/application.js
+++ public/js/application.js
@@ -1,22 +1,49 @@
-var royoApp = angular.module('royoApp', ['firebase']);
+var royoApp = angular.module('royoApp', []);
 
-royoApp.controller('royoController', ['$scope', '$filter', '$firebase', function($scope, $filter, $firebase) {
-  var peopleRef  = new Firebase("https://travislawrence.firebaseio.com/royo");
+royoApp.controller('royoController', ['$scope', '$filter', function($scope, $filter) {
+
+  var MANDRILL_API_URL = "https://mandrillapp.com/api/1.0/messages/send.json";
+
   $scope.cyclist = {type: "Cyclist", placehold: "cyclist@royo.com"}
 
-  $scope.people = $firebase(peopleRef);
   $scope.addPerson = function(email, type) {
+    var data = {
+      "key": "k9bpSuEP5O5asRVySeGc1A",
+      "message": {
+        "html": "<p>Example HTML content</p>",
+        "text": "Example text content",
+        "subject": "example subject",
+        "from_email": "message.from_email@example.com",
+        "from_name": "Example Name",
+        "to": [
+        {
+          "email": "travis.lawrence12@gmail.com",
+          "name": "Recipient Name",
+          "type": "to"
+        }
+        ],
+        "headers": {
+          "Reply-To": "message.reply@example.com"
+        }
+      },
+      "async": false,
+      "ip_pool": "Main Pool"
+    };
+
     if(!email || !type){
-      if(!email){
-        console.log('email missing!')
-      };
-      if(!type) {
-        console.log('type missing!')
-      };
+      if(!email){ console.log('email missing!') };
+      if(!type) { console.log('type missing!') };
       return;
     }else{
-      $scope.people.$add({email: $scope.rider.email, type: $scope.rider.type});
-      $scope.newPerson = "";
+      $.post(MANDRILL_API_URL, data)
+      .success(function(){
+        console.log('email sent!')
+      })
+      .fail(function(){
+        console.log('email FAIL!')
+      })
     }
   }
+
+
 }]);
