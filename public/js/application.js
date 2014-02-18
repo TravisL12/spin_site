3af0ef3ba5739013865f1160338c4764925efc7e
Travis Lawrence <travis.lawrence12@gmail.com>
2014-02-17 16:37:51 -0800

    update mailing

--- public/js/application.js
+++ public/js/application.js
@@ -2,7 +2,12 @@ var royoApp = angular.module('royoApp', []);
 
 royoApp.controller('royoController', ['$scope', '$filter', '$sce', function($scope, $filter, $sce) {
 
-  var MANDRILL_API_URL  = "https://mandrillapp.com/api/1.0/messages/send.json";
+  // var MANDRILL_API_URL  = "https://mandrillapp.com/api/1.0/messages/send.json";
+  var MAILCHIMP_API_URL = "https://us3.api.mailchimp.com/2.0/lists/subscribe.json";
+
+  $.getJSON("public/chimp.json").always(function(data) {
+    mailchimp_api = data.chimp_api;
+  });
 
   $scope.slides = [
   {title: 'Ride 1', src: "public/img/ride_through_oakland.png"},
@@ -10,41 +15,61 @@ royoApp.controller('royoController', ['$scope', '$filter', '$sce', function($sco
   ]
 
   $scope.addPerson = function(email, type) {
-    var data = {
-      "key": "k9bpSuEP5O5asRVySeGc1A",
-      "message": {
-        "html": "<p>Welcome Royo Rider type: " + type + "</p>",
-        "text": "Example text content",
-        "subject": type + " Rider",
-        "from_email": "DoNotReply@royorides.com",
-        "from_name": "Royo Rides",
-        "to": [
-        {
-          "email": email,
-          "type": "to"
-        }
-        ]
-      },
-      "async": false,
-      "ip_pool": "Main Pool"
+
+    var chimp_data = {
+      "apikey": mailchimp_api,
+      "id": "4610010e8b",
+      "email": email,
+      "merge_vars": [],
+      "email_type": "html",
+      "double_optin": false,
+      "update_existing": false,
+      "replace_interests": false,
+      "send_welcom": false
     };
+
+    // var mand_data = {
+    //   "key": "k9bpSuEP5O5asRVySeGc1A",
+    //   "message": {
+    //     "html": "<p>Welcome Royo Rider type: " + type + "</p>",
+    //     "text": "Example text content",
+    //     "subject": type + " Rider",
+    //     "from_email": "DoNotReply@royorides.com",
+    //     "from_name": "Royo Rides",
+    //     "to": [
+    //     {
+    //       "email": email,
+    //       "type": "to"
+    //     }
+    //     ]
+    //   },
+    //   "async": false,
+    //   "ip_pool": "Main Pool"
+    // };
 
     if(!email || !type){
       if(!email){ console.log('email missing!') };
       if(!type) { console.log('type missing!') };
       return;
     }else{
-      $.post(MANDRILL_API_URL, data)
+
+      $.post(MAILCHIMP_API_URL, chimp_data)
       .success(function(){
         console.log('email sent!');
-        $scope.email_success = true;
-        $scope.$apply();
       })
       .fail(function(){
         console.log('email FAIL!')
       })
-    }
-  }
 
-
+      // $.post(MANDRILL_API_URL, mand_data)
+      // .success(function(){
+      //   console.log('email sent!');
+      //   $scope.email_success = true;
+      //   $scope.$apply();
+      // })
+      // .fail(function(){
+      //   console.log('email FAIL!')
+      // })
+}
+}
 }]);
